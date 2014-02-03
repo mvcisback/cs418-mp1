bf874d18da9e028b864d00704fda4a2897242217
Marcell Vazquez-Chanlatte <mvc@linux.com>
2014-02-03 00:19:42 -0600

    added animation

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 import Graphics.UI.GLUT
 import Linear
+import Data.IORef
 
 delta = 1/4 :: GLfloat
 dx = V3 delta 0 0
@@ -8,44 +9,45 @@ o = (V3 0 0 1)
 b = o - 1.5*dx + 2*dy
 a = b + dy
 c = b + dx
+d = b + 2*dx
+e = b + 3*dx
+f = a + 3*dx
+g = c - 2*dy
+h = d - 2*dy
+i = b - 2*dy
+j = e - 2*dy
+k = i - dy
+l = j - dy
 
-reflectX :: M33 GLfloat
-reflectX = V3 (V3 (-1) 0 0) (V3 0 1 0) (V3 0 0 1)
+myPoints :: [V3 GLfloat]    
+myPoints = [a, b, c, g, i, k, l, j, h, d, e, f]
+genPoints t = map move myPoints
+    where move p = (wiggle t p) !* p
 
-reflectY :: M33 GLfloat
-reflectY = V3 (V3 1 0 0) (V3 0 (-1) 0) (V3 0 0 1)
-
--- Wiggle rates
-kX = 5
-kY = 5
-wX = 1
-wY = 1
-
+w = 1
 wiggle :: GLfloat -> V3 GLfloat -> M33 GLfloat
-wiggle t (V3 x y _) = V3 (V3 1 0 dx') (V3 0 1 dy') (V3 0 0 1)
-    where offset r k w = sin (k*r+w*t)
-          dx' = offset x kX wX
-          dy' = offset y kY wY
-          
-    
-base :: [V3 GLfloat]    
-base = [a, b, c]
-quad = base ++ map (reflectX !*) base
-myPoints = quad ++ map (reflectY !*) quad
-myPoints2 = map move myPoints
-    where move point = (wiggle 0 point) !* point
+wiggle t (V3 x y _) = V3 (V3 1 0 offset) (V3 0 1 offset) (V3 0 0 1)
+    where offset = (sin (w*t)) * x * y
 
  
 main :: IO ()
 main = do
   (_progName, _args) <- getArgsAndInitialize
   _window <- createWindow "MP1"
-  displayCallback $= display
+  tp <- newIORef (0 :: GLfloat)
+  displayCallback $= display tp
+  idleCallback $= Just (idle tp)
   mainLoop
  
-display :: DisplayCallback
-display = do 
+idle :: IORef GLfloat -> IdleCallback
+idle tp = do
+  tp $~! (+ 0.001)
+  postRedisplay Nothing
+
+display :: IORef GLfloat-> DisplayCallback
+display tp = do 
   clear [ColorBuffer]
-  renderPrimitive Points $
-     mapM_ (\(V3 x y z) -> vertex $ Vertex3 x y z) myPoints2
+  t <- readIORef tp
+  renderPrimitive LineLoop $
+     mapM_ (\(V3 x y z) -> vertex $ Vertex3 x y z) $ (genPoints t)
   flush
