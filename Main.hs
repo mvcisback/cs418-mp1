c7b87ce8acc1f5d3df4431f03b393f2eb3965908
Marcell Vazquez-Chanlatte <mvc@linux.com>
2014-02-04 13:18:33 -0600

    added wire mesh render

--- Main.hs
+++ Main.hs
@@ -2,7 +2,7 @@ import Graphics.UI.GLUT
 import Linear
 import Data.IORef
 
-epsilon = 1.99
+epsilon = 1.5
 dx = V3 (1/4) 0 0
 dy = V3 0 (1/4) 0
 o = V3 0 0 1
@@ -20,21 +20,25 @@ k = o - 2*dx - 3*dy
 l = o + 2*dx - 3*dy
 
 
-myPoints :: [V3 GLfloat]    
-myPoints = [a, b, c, d, e, f, g, h, i, j, k, l]
-
 w = 1
 wiggle :: GLfloat -> V3 GLfloat -> M33 GLfloat
 wiggle t (V3 x y _) = V3 (V3 1 0 offset) (V3 0 1 offset) (V3 0 0 1)
-    where offset = (sin (w*t)) * x * y
- 
+    where offset = (sin (w*t)) * x * y 
 
 render primative points t = renderPrimitive primative $
                             mapM_ (\(V3 x y z) -> vertex $ Vertex3 x y z) (genPoints t)
     where genPoints t = map move points
               where move p = (wiggle t p) !* p
 
-render1 = render TriangleStrip myPoints
+render1 = render TriangleStrip points
+          where points :: [V3 GLfloat]
+                points = [a, b, c, d, e, f, g, h, i, j, k, l]
+
+render2 = render Lines points
+          where points :: [V3 GLfloat]
+                points = [a, b, a, c , b, c , b, d, c, d, c, e, d, e , d, f , e
+                         ,f , e, g, f, h, g, h, g, i, h, i, h, j, i, j, i, k
+                         ,j , k, j, l, k, l]
 
 main :: IO ()
 main = do
@@ -54,5 +58,5 @@ display :: IORef GLfloat-> DisplayCallback
 display tp = do 
   clear [ColorBuffer]
   t <- readIORef tp
-  render1 t
+  render2 t
   swapBuffers
